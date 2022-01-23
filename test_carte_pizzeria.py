4d962fdd24acafb79c54b9dc89265bfe9e89ffb8
Gosselin <pro.arthur.gosselin@gmail.com>
2022-01-23 16:14:01 +0100

    test n4 avec couverture >>>>>80%

--- test_carte_pizzeria.py
+++ test_carte_pizzeria.py
@@ -22,6 +22,42 @@ def test_carte_pizza_nb_pizzas_with_no_pizzas():
     c = CartePizzeria()
     assert c.nb_pizzas() == 0
 
-#slt les anzes
+@patch('carte_pizzeria.CartePizzeria.pizzas', new_callable=PropertyMock)
+def test_carte_pizza_nb_pizzas_with_multiple_pizzas(mock_pizzas):
+    c = CartePizzeria()
+    element = Mock()
+    mock_pizzas.return_value = [element, element]
+    assert c.nb_pizzas() == 2
+
+
+def test_carte_pizza_add_pizza():
+    c = CartePizzeria()
+    pizza = Mock()
+    c.add_pizza(pizza)
+
+
+@patch('carte_pizzeria.CartePizzeria.pizzas', new_callable=PropertyMock)
+def test_carte_pizza_remove_pizza(mock_pizzas):
+    c = CartePizzeria()
+    pizza_name = "Mocked Pizza"
+    pizza = Mock()
+    pizza.name = pizza_name
+    mock_pizzas.return_value = [pizza]
+    c.remove_pizza(pizza_name)
+
+
+@patch('carte_pizzeria.CartePizzeria.pizzas', new_callable=PropertyMock)
+def test_carte_pizza_remove_pizza_failure(mock_pizzas):
+    c = CartePizzeria()
+    pizza_name = "Mocked Pizza"
+    pizza = Mock()
+    pizza.name = pizza_name + "X"
+    mock_pizzas.return_value = [pizza]
+    try:
+        c.remove_pizza(pizza_name)
+    except CartePizzeriaException:
+        pass
+    else:
+        raise Exception("test should have failed")
 
 
